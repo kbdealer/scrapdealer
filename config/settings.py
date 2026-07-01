53d4518f3a3a47f5f24e2546e06fd895000f2d8a
pyth learn <kbdealer687@gmail.com>
2026-07-01 16:49:29 +0500

    Fix ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,14 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = [
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME', ''),
+    'localhost',
+    '127.0.0.1',
+]
+
+    
 # Add this to settings.py
 LOGIN_URL = 'login'
 
